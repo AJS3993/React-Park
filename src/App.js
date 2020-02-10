e4135855dbe32e6d6bf28e5332fbdbab223d83e7
Andrew Smith <ajs3993@gmail.com>
2020-02-09 18:55:44 -0600

    nav title styling

--- src/App.js
+++ src/App.js
@@ -11,52 +11,46 @@ import Tickets from './Tickets/Tickets';
 export default function BasicExample() {
   return (
     <Router>
-      <div>
-        
+      <div className='nav'>
+
         <ul>
+          
           <li>
             <Link to="/">Park</Link>
           </li>
-          
-          <li>
-            <Link to="/tickets">Tickets</Link>
-          </li>
-          
+
           <li>
             <Link to="/attractions">Attractions</Link>
           </li>
+          
+          <li className='navRight'>
+            <Link to="/tickets">Tickets</Link>
+          </li>
 
-          <li>
+          <li className='navRight'>
             <Link to="/shop">Shop</Link>
           </li>
 
-          <li>
+          <li className='navRight'>
             <Link to="/hotels">Hotels</Link>
           </li>
         
         </ul>
+      
+      
 
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           
           <Route exact path="/">
             <Park />
           </Route>
+
+          <Route path="/attractions">
+            <Attractions />
+          </Route>
           
           <Route path="/tickets">
             <Tickets />
-          </Route>
-          
-          <Route path="/attractions">
-            <Attractions />
           </Route>
 
           <Route path="/shop">
@@ -70,26 +64,6 @@ export default function BasicExample() {
         </Switch>
       </div>
     </Router>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
   );
 }
 
