65796e466168bbfb5d0a88de3f4d05f1346e4d60
Andrew Smith <ajs3993@gmail.com>
2020-02-21 18:48:49 -0600

    background color

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Navbar from './Components/Navbar/Navbar'
 class App extends Component {
     render(){
         return(
-            <>
+            <div style={{
+                backgroundColor: 'rgb(245,255,245)'}}>
                 <Navbar/>
-            </>
+            </div>
         )
     }
 }
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,9 +4,9 @@ import { Card, Button } from 'react-bootstrap';
 function NavCard(props){
 
     return(
-        <Card>
+        <Card bg="secondary" text="white">
            <Card.Img variant="top" src={props.img}/>           
-           <Card.Title>{props.title}</Card.Title>
+           <Card.Header>{props.title}</Card.Header>
            <Card.Text>{props.text}</Card.Text>
            <Button variant="primary">{props.button}</Button>
         </Card>
